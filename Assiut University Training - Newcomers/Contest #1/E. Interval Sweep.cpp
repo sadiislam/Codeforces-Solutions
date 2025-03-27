cc92fa66f9271998631f943e195548d4a921a32b
Md. Sadiqul Islam <sadiqul.zeehad1@gmail.com>
2025-03-28 01:51:41 +0600

    Add files via upload

---
+++ Assiut University Training - Newcomers/Contest #1/E. Interval Sweep.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    cin >> a >> b;
+    if(a-b==1 || b-a==1 || (a==b && a!=0 && b!=0)) cout << "YES\n";
+    else cout << "NO\n";
+}
